6af6c6d2f16a8c13e10e355f9934e05e477d56f3
valaaryan22 <valaaryan22@gmail.com>
2025-02-13 13:09:31 +0530

    add payment

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,34 @@ import LoginForm from './components/LoginForm';
 import ProtectedRoute from './components/ProtectedRoute';
 import RegisterForm from './components/RegisterForm';
 import UpdateFAQ from './components/UpdateFAQ';
+import PaymentPage from './components/PaymentPage'; // Import the PaymentPage component
 import './i18n';
+
 function App() {
-  return (<>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        
-        {/* Protect /home route */}
-        <Route path="/home" element={<ProtectedRoute component={HomePage} />} />
-        
-        {/* Catch-all route for 404 errors */}
-        <Route path="*" element={<div>404 - Not Found</div>} />
-        
-        {/* FAQ Routes */}
-        <Route path="/faqs" element={<FAQList />} />
-        <Route path="/add-faq" element={<AddFAQ />} />
-        <Route path="/update-faq/:faqId" element={<UpdateFAQ />} />
-        <Route path="/delete-faq/:id" element={<DeleteFAQ />} />
-      </Routes>
-    </Router>
-    
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+
+          {/* Protect /home route */}
+          <Route path="/home" element={<ProtectedRoute component={HomePage} />} />
+
+          {/* Add the PaymentPage route */}
+          <Route path="/payment" element={<PaymentPage />} />
+
+          {/* Catch-all route for 404 errors */}
+          <Route path="*" element={<div>404 - Not Found</div>} />
+
+          {/* FAQ Routes */}
+          <Route path="/faqs" element={<FAQList />} />
+          <Route path="/add-faq" element={<AddFAQ />} />
+          <Route path="/update-faq/:faqId" element={<UpdateFAQ />} />
+          <Route path="/delete-faq/:id" element={<DeleteFAQ />} />
+        </Routes>
+      </Router>
     </>
-    
   );
 }
 
